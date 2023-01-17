33e60de9ff566e871dc403666dd363f3b70bc48e
Dreenvski332 <m.stanulewicz332@gmail.com>
2023-01-17 23:22:48 +0100

    API Connection and search button

--- app/src/main/java/com/example/weapp/MainActivity.java
+++ app/src/main/java/com/example/weapp/MainActivity.java
@@ -1,22 +1,12 @@
 package com.example.weapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -25,17 +15,14 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 import com.squareup.picasso.Picasso;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView degreeStatus, conditionsStatusText;
-    private TextInputEditText cityName;
+    private TextInputEditText cityNameEdit;
     private ImageView conditionsStatusImage, searchIcon, backIcon, forwardIcon;
 
     @Override
@@ -45,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         degreeStatus = findViewById(R.id.degreeStatusID);
         conditionsStatusText = findViewById(R.id.conditionsStatusTextID);
-        cityName = findViewById(R.id.textInputEditTextID);
+        cityNameEdit = findViewById(R.id.textInputEditTextID);
         conditionsStatusImage = findViewById(R.id.conditionsStatusImageID);
         searchIcon = findViewById(R.id.searchButtonID);
         backIcon = findViewById(R.id.backArrowID);
@@ -70,30 +57,33 @@ public class MainActivity extends AppCompatActivity {
         searchIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                String city = cityName.getText().toString();
-
-
+                String city = cityNameEdit.getText().toString();
+                    if(city.isEmpty()){
+                        Toast.makeText(MainActivity.this, "Please enter city name", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        collectDataFromAPI(city);
+                    }
             }
-        });
 
+        });
     }
 
-
     private void collectDataFromAPI(String cityNameAPI) {
-        String urlAPI = "http://api.weatherapi.com/v1/current.json?key=ca6f6cd999ed4563bf2181624231701&q="+ cityNameAPI +"&aqi=no";
-        cityName.setText(cityNameAPI);
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=090a784c78ff437d89f220953231701&q=" + cityNameAPI + "&days=1&aqi=no&alerts=no";
+        cityNameEdit.setText(cityNameAPI);
         RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlAPI, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
                     String temp = response.getJSONObject("current").getString("temp_c");
-                    degreeStatus.setText(temp + "°C");
+                    degreeStatus.setText(temp);
                     String cond = response.getJSONObject("current").getJSONObject("condition").getString("text");
+                    conditionsStatusText.setText(cond);
                     String condIcon = response.getJSONObject("current").getJSONObject("condition").getString("icon");
                     Picasso.get().load("http:".concat(condIcon)).into(conditionsStatusImage);
-                    conditionsStatusText.setText(cond);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -104,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Enter valid city name", Toast.LENGTH_SHORT).show();
             }
         });
-
         requestQueue.add(jsonObjectRequest);
     }
 }
